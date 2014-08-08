07d617ec47126e7ff0bdc170140768b8995a0773
C.C.T <changcheng.tu@gmail.com>
2014-08-08 20:15:48 +0800

    remove mynote

--- editnote/static/ckeditor/config.js
+++ editnote/static/ckeditor/config.js
@@ -7,11 +7,4 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.language = 'zh';
-	config.extraPlugins = 'autosave';
-	config.autosave_SaveKey = 'autosaveKey';
-	config.autosave_NotOlderThan = 1;
-	config.autosave_saveOnDestroy = false;
-	config.autosaveTargetUrl = 'http://example.com/path_to_script_that_saves_data'; 
-	
 };
